a6195b369774907420391f95f1c8a3905e113e7d
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-08-05 12:24:14 -0700

    changed GA code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { NotificationsProvider } from "@mantine/notifications";
 import { sizes } from "../helpers";
 import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
+import Script from "next/script";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -33,9 +34,20 @@ export default function App(props: AppProps) {
         }}
       >
         <NotificationsProvider position="bottom-center" zIndex={2077}>
-          <GoogleAnalytics
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_CODE}`}
+          ></Script>
+          <Script>
+            {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_CODE}');`}
+          </Script>
+          {/* <GoogleAnalytics
             gaMeasurementId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_CODE}
-          />
+          /> */}
           <Component {...pageProps} />
         </NotificationsProvider>
       </MantineProvider>
